6e085db3e5f224c171aa9161c0fd959f85b7f830
Darren Regan <darren111894@gmail.com>
2019-10-23 15:10:04 +0100

    Password Client & Server Starter Code

--- src/main/java/ie/gmit/ds/PasswordServer.java
+++ src/main/java/ie/gmit/ds/PasswordServer.java
@@ -1,4 +1,55 @@
 package ie.gmit.ds;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class PasswordServer {
-}
+    private Server server;
+    private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new HelloWorldServer.GreeterImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+        @Override
+        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+
+    }
