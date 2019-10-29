eaa91cf816929fe05a7ee098b00c9f904b0ebe79
Darren Regan <darren111894@gmail.com>
2019-10-29 03:27:42 +0000

    Update PsImpl

--- src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -6,11 +6,29 @@ import io.grpc.stub.StreamObserver;
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase{
 
     private static final Logger logger = Logger.getLogger(PasswordServiceImpl.class.getName());
+
+    //Variables
+    String password;
+    int userId;
+    char passwordChar;
+    byte hashedPassword;
+    byte salt;
+    boolean validatePassword;
+
+    @Override
     public void validate(){
 
     }
 
+    @Override
     public void hash(){
+
+        //Request password from user
+        //Get userId and Salt
+
+        //Hash Password
+
+        //print info
 
     }
 }
